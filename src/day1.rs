497ba607ebce4746d64e35db7ccf478b2741fc2f
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2020-12-01 12:33:29 +0100

    day 1 part 1

---
+++ src/day1.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn day1_part1() -> i32 {
+    println!("{:?}", read());
+
+    for num1 in read().iter() {
+        for num2 in read().iter() {
+            if num1 + num2 == 2020 {
+                println!("num1: {} num2: {}", num1, num2);
+                return num1*num2
+            }
+        }
+    }
+
+    return -1
+}
+
+
+fn read() -> Vec<i32> {
+    let br = BufReader::new(File::open("src/day1/input").expect("No such file"));
+    return br.lines()
+        .map(|line| line.expect("sadf").trim().parse().expect("cannot parse line"))
+        .collect();
+}
+
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod day1;
+
+fn main() {
+    println!("Hello, world!");
+
+    let x = day1::day1_part1();
+    println!("day1 part 1: {:?}", x)
+}
