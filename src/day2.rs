88cd742e2c0217427eb06002d07df735a6c1cd2b
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2020-12-02 22:07:21 +0100

    simplify

--- src/day2.rs
+++ src/day2.rs
@@ -16,29 +16,16 @@ pub fn day2_part2() -> i32 {
 }
 
 fn is_valid(line: String) -> bool {
-    let v = line.split(":").collect::<Vec<_>>();
-    let range = line.split(":").nth(0).expect("Must exist").split(" ").nth(0).expect("Must exist");
-    let letter = line.split(":").nth(0).expect("Must exist").split(" ").nth(1).expect("Must exist");
     
-    let min_occurrences = range.split("-").nth(0).expect("Must exist").parse().expect("Must be a number");
-    let max_occurrences = range.split("-").nth(1).expect("Must exist").parse().expect("Must be a number");
+    let (min_occurrences, max_occurrences, letter, password) = line_to_tuple(line);
 
-    let password = v[1];
-
-    let num_matches_letter = password.chars().filter(|c| *c == letter.chars().nth(0).expect("should be char")).count();
+    let num_matches_letter = password.chars().filter(|c| *c == letter).count();
 
     return num_matches_letter >= min_occurrences && num_matches_letter <= max_occurrences;
 }
 
 fn is_valid2(line: String) -> bool {
-    let v = line.split(":").collect::<Vec<_>>();
-    let range = line.split(":").nth(0).expect("Must exist").split(" ").nth(0).expect("Must exist");
-    let letter = line.split(":").nth(0).expect("Must exist").split(" ").nth(1).expect("Must exist").chars().nth(0).expect("exist");
-    
-    let first_occurrence = range.split("-").nth(0).expect("Must exist").parse().expect("Must be a number");
-    let last_occurrence = range.split("-").nth(1).expect("Must exist").parse().expect("Must be a number");
-
-    let password = v[1];
+    let (first_occurrence, last_occurrence, letter, password) = line_to_tuple(line);
 
     let first_matches = char_equals(password.to_string(), first_occurrence, letter);
     let last_matches = char_equals(password.to_string(), last_occurrence, letter);
@@ -53,7 +40,17 @@ fn char_equals(password: String, index: usize, c: char) -> bool {
     }
 }
 
+fn line_to_tuple(line: String) -> (usize, usize, char, String) {
+    let left = line.split(":").nth(0).expect("Must exist");
+    let password = line.split(":").nth(1).expect("Must exist");
+    let range = left.split(" ").nth(0).expect("Must exist");
+    let letter = left.split(":").nth(0).expect("Must exist").split(" ").nth(1).expect("Must exist").chars().nth(0).expect("exist");
+    
+    let first_occurrence = range.split("-").nth(0).expect("Must exist").parse().expect("Must be a number");
+    let last_occurrence = range.split("-").nth(1).expect("Must exist").parse().expect("Must be a number");
 
+    return (first_occurrence, last_occurrence, letter, password.to_string());
+}
 
 fn read(path: String) -> Vec<String> {
     let br = BufReader::new(File::open(path).expect("No such file"));
