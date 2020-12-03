d1ad1e15fcf5c0abb3935c9eb436e5b63039fb76
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2020-12-03 08:51:47 +0100

    use borrowing instead of new strings

--- src/day2.rs
+++ src/day2.rs
@@ -3,19 +3,19 @@ use std::io::{BufRead, BufReader};
 
 pub fn day2_part1() -> i32 {
 
-    let lines = read("data/day2/input".to_string());
+    let lines = read("data/day2/input");
     
-    return lines.iter().filter(|line| is_valid(line.to_string())).count() as i32;
+    return lines.iter().filter(|line| is_valid(line)).count() as i32;
 }
 
 pub fn day2_part2() -> i32 {
 
-    let lines = read("data/day2/input".to_string());
+    let lines = read("data/day2/input");
     
-    return lines.iter().filter(|line| is_valid2(line.to_string())).count() as i32;
+    return lines.iter().filter(|line| is_valid2(line)).count() as i32;
 }
 
-fn is_valid(line: String) -> bool {
+fn is_valid(line: &str) -> bool {
     
     let (min_occurrences, max_occurrences, letter, password) = line_to_tuple(line);
 
@@ -24,15 +24,15 @@ fn is_valid(line: String) -> bool {
     return num_matches_letter >= min_occurrences && num_matches_letter <= max_occurrences;
 }
 
-fn is_valid2(line: String) -> bool {
+fn is_valid2(line: &str) -> bool {
     let (first_occurrence, last_occurrence, letter, password) = line_to_tuple(line);
 
-    let first_matches = char_equals(password.to_string(), first_occurrence, letter);
-    let last_matches = char_equals(password.to_string(), last_occurrence, letter);
+    let first_matches = char_equals(password, first_occurrence, letter);
+    let last_matches = char_equals(password, last_occurrence, letter);
     return first_matches && !last_matches || !first_matches && last_matches;
 }
 
-fn char_equals(password: String, index: usize, c: char) -> bool {
+fn char_equals(password: &str, index: usize, c: char) -> bool {
     if password.len() <= index {
         false
     } else {
@@ -40,7 +40,7 @@ fn char_equals(password: String, index: usize, c: char) -> bool {
     }
 }
 
-fn line_to_tuple(line: String) -> (usize, usize, char, String) {
+fn line_to_tuple(line: &str) -> (usize, usize, char, &str) {
     let left = line.split(":").nth(0).expect("Must exist");
     let password = line.split(":").nth(1).expect("Must exist");
     let range = left.split(" ").nth(0).expect("Must exist");
@@ -49,10 +49,10 @@ fn line_to_tuple(line: String) -> (usize, usize, char, String) {
     let first_occurrence = range.split("-").nth(0).expect("Must exist").parse().expect("Must be a number");
     let last_occurrence = range.split("-").nth(1).expect("Must exist").parse().expect("Must be a number");
 
-    return (first_occurrence, last_occurrence, letter, password.to_string());
+    return (first_occurrence, last_occurrence, letter, password);
 }
 
-fn read(path: String) -> Vec<String> {
+fn read(path: &str) -> Vec<String> {
     let br = BufReader::new(File::open(path).expect("No such file"));
     return br.lines()
         .map(|line| line.expect("Expect a valid String"))
