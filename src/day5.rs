bfcc3b9f7262e95f4560e836853893bf4da37b6c
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2020-12-06 22:11:02 +0100

    day5 part1

--- src/day5.rs
+++ src/day5.rs
@@ -6,17 +6,31 @@ mod tests {
 
     #[test]
     fn test_day5_part1_example() {
-        assert_eq!(day5_part1("FBFBBFF"), 44);
+        assert_eq!(get_seat_id("FBFBBFFRLR"), 357);
+        assert_eq!(get_seat_id("BFFFBBFRRR"), 567);
+        assert_eq!(get_seat_id("FFFBBBFRRR"), 119);
+        assert_eq!(get_seat_id("BBFFBBFRLL"), 820);
     }
+
+    #[test]
+    fn test_day5_part1() {
+
+        let max_seat_id = read("data/day5/input").map(|code| get_seat_id(&code)).max().unwrap();
+        assert_eq!(max_seat_id, 818);
+    }
+
+
 }
 
-pub fn day5_part1(code: &str) -> usize {
-    bin_space_partition(code, 1, 128, 'B', 'F')
+pub fn get_seat_id(code: &str) -> usize {
+    bin_space_partition(&code[..7], 1, 128) * 8 + bin_space_partition(&code[7..], 1, 8)
 }
 
-fn bin_space_partition(code: &str, lower: i32, upper: i32, lower_char: char, upper_char: char) -> usize {
+fn bin_space_partition(code: &str, lower: i32, upper: i32) -> usize {
 
     let half = Half::new(code.chars().nth(0).unwrap());
+
+    println!("code: {}, half: {:?}, lower: {}, upper: {}", code, half, lower, upper);
 
     if code.len() == 1 {
         match half {
@@ -27,10 +41,10 @@ fn bin_space_partition(code: &str, lower: i32, upper: i32, lower_char: char, upp
         match half {
             Half::Upper => bin_space_partition(&code[1..], 
                 lower + 2_i32.pow((code.len() - 1) as u32),
-                upper, lower_char, upper_char),
+                upper),
             Half::Lower => bin_space_partition(&code[1..], 
                 lower,
-                upper - 2_i32.pow((code.len() - 1) as u32), lower_char, upper_char),
+                upper - 2_i32.pow((code.len() - 1) as u32)),
         }
     }
 }
