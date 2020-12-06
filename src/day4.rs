baaabac816c88d1a38b40bb106163a8d0ebed1d5
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2020-12-06 02:54:40 +0100

    day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -12,13 +12,24 @@ mod tests {
     fn test_day4_part1() {
         assert_eq!(day4_part1("data/day4/input"), 230);
     }
+
+    #[test]
+    fn test_day4_part2() {
+        assert_eq!(day4_part2("data/day4/input"), 156);
+    }
 }
 
 pub fn day4_part1(path: &str) -> usize {
-    read_between_lines(path).for_each(|x| println!("{}", x));
     let map = read_between_lines(path);
     map.map(|line| Passport::new(&line))
         .filter(|p| p.is_valid())
+        .count()
+}
+
+pub fn day4_part2(path: &str) -> usize {
+    let map = read_between_lines(path);
+    map.map(|line| Passport::new(&line))
+        .filter(|p| p.is_valid2())
         .count()
 }
 
@@ -61,6 +72,81 @@ impl Passport {
     fn is_valid(&self) -> bool {
         [&self.byr, &self.iyr, &self.eyr, &self.hgt, &self.hcl, &self.ecl, &self.pid].iter().all(|field| field.is_some())
     }
+
+    fn is_valid2(&self) -> bool {
+        
+
+        if self.is_valid() {
+            byr_valid(&self.byr.as_ref().unwrap()) &&
+                iyr_valid(&self.iyr.as_ref().unwrap()) &&
+                eyr_valid(&self.eyr.as_ref().unwrap()) &&
+                hgt_valid(&self.hgt.as_ref().unwrap()) &&
+                hcl_valid(&self.hcl.as_ref().unwrap()) &&
+                ecl_valid(&self.ecl.as_ref().unwrap()) &&
+                self.pid.as_ref().unwrap().chars().filter(|c| c.is_digit(10)).count() == 9
+       } else {
+            false
+        }
+    }
+
+}
+
+fn byr_valid(text: &str) -> bool {
+    x_digits_min_max(text, 4, 1920, 2002)
+}
+
+fn iyr_valid(text: &str) -> bool {
+    x_digits_min_max(text, 4, 2010, 2020)
+}    
+
+fn eyr_valid(text: &str) -> bool {
+    x_digits_min_max(text, 4, 2020, 2030)
+}
+
+fn hgt_valid(text: &str) -> bool {
+    if text.len() < 4 {
+        return false;
+    }
+
+    match &text[text.len() - 2..] {
+        "cm" => x_digits_min_max(&text[..3], 3, 150, 193),
+        "in" => x_digits_min_max(&text[..2], 2, 59, 76),
+        _ => {
+            println!("hgt invalid {}", text);
+            false
+        } 
+    }
+}
+
+fn hcl_valid(text: &str) -> bool {
+    if text.len() != 7 {
+        return false;        
+    }
+
+    if &text[..1] != "#" {
+        return false;
+    }
+
+    text[1..].chars().all(|c| c.is_digit(16))
+}
+
+fn ecl_valid(text: &str) -> bool {
+    match text {
+        "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
+        _ => false
+    }
+}
+
+fn x_digits_min_max(text: &str, num: usize, min: usize, max: usize) -> bool {
+    if text.chars().all(|c| c.is_digit(10)) &&
+        text.len() == num &&
+        text.parse::<usize>().unwrap() >= min &&
+        text.parse::<usize>().unwrap() <= max {
+            true
+        } else {
+            println!("invalid {}", text);
+            false
+        }
 }
 
 fn get_val_for_key(line: &str, key: &str) -> Option<String> {
