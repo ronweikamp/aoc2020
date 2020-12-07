33a968c313edd790d9fec75ed6d5202ec94615a1
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2020-12-07 20:34:44 +0100

    day7

---
+++ src/day7.rs
@@ -0,0 +1,111 @@
+use petgraph::prelude::Dfs;
+use std::collections::HashMap;
+use petgraph::graph::NodeIndex;
+use petgraph::stable_graph::StableGraph;
+use petgraph::prelude::GraphMap;
+use petgraph::Graph;
+use std::collections::HashSet;
+use crate::utils::read;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day7_part1_example() {
+        assert_eq!(day7_part1("data/day7/example"), 4);
+    }
+
+    #[test]
+    fn test_day7_part1() {
+        assert_eq!(day7_part1("data/day7/input"), 144);
+    }
+
+
+}
+
+pub fn day7_part1(path: &str) -> usize {
+    let mut bag_graph = Graph::<&str, u32>::new();
+
+    let mut str_to_node = HashSet::<String>::new();
+
+    read(path).for_each(|l| {
+        let (node, connections) = line_to_bags(&l);
+        
+        str_to_node.insert(node.to_string());
+        for c in connections {
+            str_to_node.insert(c);
+        }
+    }); 
+
+    let map: HashMap<String, NodeIndex> = str_to_node.iter().map(|s| (s.to_string(), bag_graph.add_node(s))).collect::<HashMap<String, NodeIndex>>();
+
+
+    read(path).for_each(|l| {
+        let (node, connections) = line_to_bags(&l);
+        let node_i = map.get(&node);
+        
+        for c in connections {
+            
+            println!("conn {}", c);
+            bag_graph.add_edge(*map.get(&c).unwrap(), *node_i.unwrap(), 1);
+        }
+    });
+
+
+    println!("{:?}", bag_graph);
+
+    let start = map.get("shiny gold ").unwrap();
+    let mut dfs = Dfs::new(&bag_graph, *start);
+    let mut count = 0;
+
+    while let Some(visited) = dfs.next(&bag_graph) {
+        println!("v {}", visited.index());
+        if map.get("other bags. ").unwrap().index() != visited.index() {
+            count += 1;
+        }
+    }
+
+
+    // to correct for the node itself and for
+    count - 1
+}
+
+fn line_to_bags(line: &str) -> (String, HashSet<String>) {
+
+    println!("line {}", line);
+
+    let first = line.split(',').nth(0).unwrap();
+    
+    let mut connections = HashSet::new();
+
+    let mut target = String::new();
+    first.split(' ').take_while(|&w| w != "bags").for_each(|w| {
+        target.push_str(w);
+        target.push_str(" ");
+    });
+
+    println!("target {}", target);
+
+    let mut first_content = String::new(); 
+    first.split(' ').skip(5).take(2).for_each(|w| {
+        first_content.push_str(w);
+        first_content.push_str(" ");
+    });
+
+    println!("fc {}", first_content);
+    connections.insert(first_content);
+
+    for sub_seq_contents in line.split(',').skip(1) {
+        let mut sc = String::new();
+        sub_seq_contents.split(' ').skip(2).take(2).for_each(|w| {
+            sc.push_str(w);
+            sc.push_str(" ");
+        });
+        println!("sc {}", sc);
+        connections.insert(sc);
+    }
+
+    (target, connections)  
+}
+
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod utils;
 pub mod day4;
 pub mod day5;
 pub mod day6;
+pub mod day7;
 
 fn main() {
     println!("Hello, world!");
