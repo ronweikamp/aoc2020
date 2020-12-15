eea7d136c8ba6336e89246d42e5dcfdff3ef1d0a
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2020-12-15 22:13:28 +0100

    day 13 part2

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use crate::utils::read;
 
 #[cfg(test)]
@@ -29,8 +30,13 @@ mod tests {
     fn test_day13_part2_example6() {
         assert_eq!(day13_part2("data/day13/example6"), 1202161486);
     }
+    #[test]
+    fn test_day13_part2() {
+        assert_eq!(day13_part2_input_with_crt().unwrap(), 598411311431841);
+    }
     
 }
+
 
 pub fn day13_part1(path: &str) -> usize {
 
@@ -40,21 +46,82 @@ pub fn day13_part1(path: &str) -> usize {
     t.0 * t.1
 }
 
+fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
+    if a == 0 {
+        (b, 0, 1)
+    } else {
+        let (g, x, y) = egcd(b % a, a);
+        (g, y - (b / a) * x, x)
+    }
+}
+ 
+fn mod_inv(x: i64, n: i64) -> Option<i64> {
+    let (g, x, _) = egcd(x, n);
+    if g == 1 {
+        Some((x % n + n) % n)
+    } else {
+        None
+    }
+}
+ 
+fn chinese_remainder(residues: &[i64], modulii: &[i64]) -> Option<i64> {
+    let prod = modulii.iter().product::<i64>();
+ 
+    let mut sum = 0;
+ 
+    for (&residue, &modulus) in residues.iter().zip(modulii) {
+        let p = prod / modulus;
+        sum += residue * mod_inv(p, modulus)? * p
+    }
+ 
+    Some(sum % prod)
+}
+ 
+fn day13_part2_input_with_crt() -> Option<i64> {
+    let input = [(54, 13), (58, 17), (91, 19), (64, 23), (43, 29), (35, 37), (0, 41), (72, 419), (41, 557)];
+
+    let modulii: Vec<i64> = input.iter().map(|(_, p)| *p).collect();
+    let residues: Vec<i64> = input.iter().map(|(o, p)| (p-o) % p ).collect();
+    //let modulii = [3,5];
+    //let residues = [0,4];
+ 
+    chinese_remainder(&residues, &modulii)
+}
+
 pub fn day13_part2(path: &str) -> usize {
 
     let numbers = read_input2(path);
+    let mut num_enumeration: Vec<(usize, usize)> = read_input2(path).iter().enumerate()
+        .filter(|(_, n)| *n > &(0 as usize))
+        .map(|(a,b)| (a, *b))
+        .collect();
+    num_enumeration.sort_by(|t1, t2| t1.1.cmp(&t2.1));
 
     let base = numbers.iter().nth(0).unwrap();
+    let (offset, max_p) = num_enumeration.iter().max_by(|t1, t2| t1.1.cmp(&t2.1)).unwrap();
+    print!("{:?}", num_enumeration);
+    println!("max {} {}", offset, max_p);
     let mut multiplier: usize = 1;
 
+    let mut skip: usize = 0;
+
+    let mut iteration : usize = 0;
+
     loop {
-        if numbers.iter().enumerate()
-            .filter(|(_, n)| *n > &(0 as usize))
-            .all(|(i, n)| (base * multiplier + i) % n == 0) {
-            return multiplier * base;
+        let target =multiplier * max_p - offset;
+
+        if num_enumeration.iter().all(|(i, n)| (target + i) % n == 0) {
+            println!("mult {}", multiplier);
+            println!("skip {}", skip);
+            return target;
         } else {
             multiplier = multiplier + 1;
         }
+
+        if iteration % 1000000000 == 0 {
+            println!("iter, target {}, {}", iteration, target);
+        }
+        iteration = iteration + 1;
     }
 
 
